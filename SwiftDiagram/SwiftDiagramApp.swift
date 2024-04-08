8d1d7067d27f454169f72b8a0197228612459822
Pierre Gabory <pierre.gabory@withings.com>
2024-04-08 21:52:35 +0200

    Initial Commit

---
+++ SwiftDiagram/SwiftDiagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDiagramApp.swift
+//  SwiftDiagram
+//
+//  Created by Pierre Gabory on 08/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDiagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
